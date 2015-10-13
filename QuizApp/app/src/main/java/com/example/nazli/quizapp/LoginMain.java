4ddd858ba7fbf45938ea4d8ecf17e1b06b524c6b
Nazli Medghalchi <nazlimedghalchi@Nazlis-MacBook-Pro.local>
2015-10-12 22:40:50 -0400

    QuizTaker Login fields added

--- QuizApp/app/src/main/java/com/example/nazli/quizapp/LoginMain.java
+++ QuizApp/app/src/main/java/com/example/nazli/quizapp/LoginMain.java
@@ -1,10 +1,16 @@
 package com.example.nazli.quizapp;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by nazlimedghalchi on 15-10-08.
  */
 public class LoginMain extends Activity{
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
