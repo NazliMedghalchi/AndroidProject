0e1fe068a5516470f35fb9a0b23a9cc24a67c56f
NazliMedghalchi <medghalchi.n@gmail.com>
2015-10-29 15:05:50 -0400

    Instant Messaging - used template:blank activiti with fragment

---
+++ QuizApp 2/app/src/main/java/com/example/nazli/quizapp/DatabaseHandler.java
@@ -0,0 +1,130 @@
+package com.example.nazli.quizapp;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by nazlimedghalchi on 2015-10-17.
+ */
+public class DatabaseHandler extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "quizApp",
+            TABLE_quizMaster = "quizMaster",
+            TABLE_quizTaker = "quizTaker",
+            TABLE_quiz ="quiz",
+            TABLE_question = "question",
+    // fields in quiz Master and Taker tables
+            USER_QT = "usserQT",
+            USER_QM = "userQM",
+            PASS_QT = "passQT",
+            PASS_QM = "passQM",
+    // fields in quiz table
+            QUIZ_ID = "label_quiz",
+            QUIZ_TIMER = "quiz_timer",
+            QUIZ_ATTEMPT = "quiz_attempt",
+    // quiz_length is based on number of questions
+            QUIZ_LENGHT = "quiz_length",
+    // fields in question table
+            QST_ID = "qst_id",
+            QST_TIMER = "qst_timer",
+    // qst_answr := question correct answer
+            QST_ANSWR = "qst_answr",
+            MIN_TIME = "min_time",
+            MAX_TIME = "max_time";
+
+    public DatabaseHandler(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL("CREATE TABLE " + TABLE_quizMaster + "("+ USER_QM + "STRING PRIMARY KEY," + PASS_QM + "STRING)");
+        db.execSQL("CREATE TABLE " + TABLE_quizTaker + "("+ USER_QT + "STRING PRIMARY KEY," + PASS_QT + "STRING)");
+        db.execSQL("CREATE TABLE " + TABLE_quiz + "(" + QUIZ_ID + "INTEGER PRIMARY KEY AUTOINCREAMENT," + QUIZ_TIMER + "TIME," + QUIZ_ATTEMPT
+        + "INTEGER," + QUIZ_LENGHT + "INTEGER)");
+        db.execSQL("CREATE TABLE " + TABLE_question + "(" + QST_ID + "INTEGER," + QST_TIMER + "TIME," + QST_ANSWR + "STRING,"
+                + MIN_TIME + "TIME," + MAX_TIME + "TIME)");
+    }
+    @Override
+    public void onUpgrade (SQLiteDatabase db, int oldVersion, int newVersion){
+        db.execSQL("DROP TABLE IF EXISTS  " + TABLE_quizTaker);
+        db.execSQL("DROP TABLE IF EXISTS  " + TABLE_quizMaster);
+        db.execSQL("DROP TABLE IF EXISTS  " + TABLE_quiz);
+        db.execSQL("DROP TABLE IF EXISTS  " + TABLE_question);
+    }
+    public void addQuizTaker (ContactsContract.Contacts QT){
+        SQLiteDatabase db = getWritableDatabase();
+        Map userQT = new Map() {
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public boolean containsKey(Object key) {
+                return false;
+            }
+
+            @Override
+            public boolean containsValue(Object value) {
+                return false;
+            }
+
+            @NonNull
+            @Override
+            public Set<Entry> entrySet() {
+                return null;
+            }
+
+            @Override
+            public Object get(Object key) {
+                return null;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @NonNull
+            @Override
+            public Set keySet() {
+                return null;
+            }
+
+            @Override
+            public Object put(Object key, Object value) {
+                return null;
+            }
+
+            @Override
+            public void putAll(Map map) {
+
+            }
+
+            @Override
+            public Object remove(Object key) {
+                return null;
+            }
+
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @NonNull
+            @Override
+            public Collection values() {
+                return null;
+            }
+        };
+        db.put
+
+    }
+}
---
+++ QuizApp 2/app/src/main/java/com/example/nazli/quizapp/quizTaker.java
@@ -0,0 +1,75 @@
+package com.example.nazli.quizapp;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.content.Context;
+
+/**
+ * Created by nazlimedghalchi on 2015-10-14.
+ */
+public class quizTaker extends Activity {
+
+    //UI componnets
+    private TextView lable_quiz = (TextView) findViewById(R.id.label_quiz);
+    private Chronometer chronometer_quiz = (Chronometer) findViewById(R.id.chronometer_quiz);
+    private TextView textView_question = (TextView) findViewById(R.id.textView_question);
+    private RadioButton row1_choice = (RadioButton) findViewById(R.id.row1_choice);
+    private RadioButton row2_choice = (RadioButton) findViewById(R.id.row2_choice);
+    private RadioButton row3_choice = (RadioButton) findViewById(R.id.row3_choice);
+    private RadioButton row4_choice = (RadioButton) findViewById(R.id.row4_choice);
+    private RadioButton[] guessAnswer; // row of multiple choice
+    private LinearLayout questionNumberTextView;
+    private LinearLayout linearlayoutH_result = (LinearLayout) findViewById(R.id.linearlayoutH_result);
+    private TextView score_textview = (TextView) findViewById(R.id.score_textView);
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //show quiz questions
+        lable_quiz.setText(getResources().getString(R.string.));
+        return inflater.inflate(R.layout.fragment_quiz, container, false);
+
+        // get references to GUI components
+
+        guessAnswer = new RadioButton[4];
+        guessAnswer[0] =
+                (RadioButton) findViewById(R.id.radioButton1_choice);
+        guessAnswer[1] =
+                (RadioButton) findViewById(R.id.radioButton2_choice);
+        guessAnswer[2] =
+                (RadioButton) findViewById(R.id.radioButton3_choice);
+        guessAnswer[3] =
+                (RadioButton) findViewById(R.id.radioButton4_choice);
+
+        for (RadioButton row : guessAnswer){
+            row.setText(String.valueOf(getText(toString(R.array.multiple_choice))));
+            row.setClickable(true);
+            View.OnClickListener radioButtonListener = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+
+                }
+            };
+        }
+
+
+    }
+
+    private void onCreateQuiz (){
+        lable_quiz.setText(getTex);
+    }
+}
