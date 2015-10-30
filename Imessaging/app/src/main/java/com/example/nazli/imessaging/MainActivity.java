189d023e121f23f5695d262c4f3718344284fe18
NazliMedghalchi <medghalchi.n@gmail.com>
2015-10-30 16:54:34 -0400

    Chat Layout first design

--- Imessaging/app/src/main/java/com/example/nazli/imessaging/MainActivity.java
+++ Imessaging/app/src/main/java/com/example/nazli/imessaging/MainActivity.java
@@ -1,20 +1,37 @@
 package com.example.nazli.imessaging;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.preference.DialogPreference;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+    private String usrname = "admin";
+    private String pass = "password";
+    private Button signin = (Button) findViewById(R.id.signin);
+    private EditText username = (EditText) findViewById(R.id.uname);
+    private EditText password = (EditText) findViewById(R.id.password);
+    private String alertTitle = getResources().getString(R.string.alert_title);
+    private String alertMessage = getResources().getString(R.string.alert_message);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -29,6 +46,7 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
@@ -47,7 +65,40 @@ public class MainActivity extends Activity {
             setContentView(R.layout.activity_main);
         }
 
-
         return super.onOptionsItemSelected(item);
     }
+
+    public void signIn (View view) {
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ( (username.getText().toString()
+                        == usrname) && (password.getText().toString() == pass)) {
+                    setContentView(R.layout.list_of_conversations);
+                } else {
+
+//                    // Alert DialogBox
+//                    AlertDialog.Builder failedLogin = new AlertDialog.Builder(appContext);
+//                    // Title
+//                    failedLogin.setMessage(alertTitle);
+//                    failedLogin.setMessage(alertMessage);
+//                    // cancel alert
+//                    failedLogin.setCancelable(false);
+//                    //
+//                    failedLogin.setPositiveButton("OKAY", new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialog, int id) {
+//                            dialog.cancel();
+//                        }
+//                    });
+//                    AlertDialog error = failedLogin.create();
+//                    error.show();
+                    username.setText("");
+                    password.setText("");
+                }
+            }
+        });
+
+    }
+
 }
